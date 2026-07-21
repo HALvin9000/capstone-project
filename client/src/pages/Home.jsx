680b019d99ad646a08b0b134e95788ddc14b19c6
Admin <kelvinkch56@gmail.com>
2026-07-21 17:35:56 -0400

    feat: early WIP structure

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Button from '../components/Button';
+
+function Home() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+
+      <Button text="Test" />
+    </>
+  );
+}
+
+export default Home;
